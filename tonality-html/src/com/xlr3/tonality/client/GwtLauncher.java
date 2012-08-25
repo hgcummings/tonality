5ed2a35e92e3718610b1662ab4052ad4e40e0ad1
Harry <harry@xlr3.com>
2012-08-25 10:48:28 +0100

    Initial project files

---
+++ tonality-html/src/com/xlr3/tonality/client/GwtLauncher.java
@@ -0,0 +1,19 @@
+package com.xlr3.tonality.client;
+
+import com.xlr3.tonality.TonalityGame;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+
+public class GwtLauncher extends GwtApplication {
+	@Override
+	public GwtApplicationConfiguration getConfig () {
+		GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(480, 320);
+		return cfg;
+	}
+
+	@Override
+	public ApplicationListener getApplicationListener () {
+		return new TonalityGame();
+	}
+}
